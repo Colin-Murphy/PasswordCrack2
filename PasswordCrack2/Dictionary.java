5a2bdfdb3a8b4ca2c9b10864f3ac949b6c042d42
Colin Murphy <colinmurphy1993@gmail.com>
2014-03-12 19:33:43 -0400

    Edited the original PasswordCrack.java file to begin to meet the spec of project 2

---
+++ PasswordCrack2/Dictionary.java
@@ -0,0 +1,84 @@
+/**
+	File: Dictionary.java	
+	Designed for RIT Concepts of Paralel and Distributed Systems Project 1
+	Reused in Project 2
+	
+	@author Colin L Murphy <clm3888@rit.edu>
+	@version 3/12/14
+*/
+
+
+//Backed by a hashmap
+import java.util.*;
+
+//Rit Hex Library
+import edu.rit.util.Hex;
+
+public class Dictionary {
+
+	//Hashmap backend
+	private HashMap<String, String> map;
+	//The number of items to be added
+	private int finalCount = 0;
+
+
+	/**
+	 * Construct a new dictionary.
+	 */
+	public Dictionary() {
+		 map = new HashMap<String, String>();
+
+	}
+
+	/**
+	 * Set the total number of passwords that will be added to this dictionary.
+	 *
+	 * @param  count  Total number of passwords.
+	 */
+	public synchronized void setCount(int count) {
+		finalCount = count;
+		
+	}
+
+	/**
+	 * Add the given password and its digest to this dictionary.
+	 *
+	 * @param  password  Password.
+	 * @param  digest	 Password digest.
+	 */
+	public synchronized void add(String password, byte[] digest) {
+		String hex = Hex.toString(digest);
+		map.put(hex, password);
+		notifyAll();
+		
+	}
+
+	/**
+	 * Get the password for the given digest. This method blocks the calling
+	 * thread until either (a) a password with the given digest is added to this
+	 * dictionary, or (b) all passwords have been added to this dictionary. Then
+	 * this method returns either (a) the password for the given digest if the
+	 * digest was found, or (b) null if the digest was not found.
+	 *
+	 * @param  digest  Password digest.
+	 *
+	 * @return  Password, or null.
+	 */
+	public synchronized String get (byte[] digest)
+		throws InterruptedException {
+		
+		String hex = Hex.toString(digest);
+		//Wait until the password is found, or all hashes are done
+		while(map.size() < finalCount && !map.containsKey(hex)) {
+			wait();
+		}
+		
+		//Print the password if it was found
+		if (map.containsKey(hex)) {
+			return map.get(hex);
+		}
+		//Not found, return null
+		return null;
+	
+	}
+}
---
+++ PasswordCrack2/PasswordCrack2.java
@@ -0,0 +1,111 @@
+/**
+	File: PasswordCrack.java	
+	Designed for RIT Concepts of Paralel and Distributed Systems Project 1
+	
+	@author Colin L Murphy <clm3888@rit.edu>
+	@version 3/5/14
+*/
+
+
+//Read files
+import java.io.File;
+//Arraylist for storing users and hashed passwords
+import java.util.ArrayList;
+//Buffered reader
+import java.io.BufferedReader;
+//File reader
+import java.io.FileReader;
+//Io exception
+import java.io.IOException;
+//File not found
+import java.io.FileNotFoundException;
+
+
+
+
+/**
+ * Class PasswordCrack is the password cracking main program.
+ */
+public class PasswordCrack2 {
+	
+	//The number of  characters to check (a-z) (0-9)
+	private static final int CHARACTERS = 36
+	
+	//The maximum password length
+	private static final int LENGTH = 4;
+
+	/**
+	 * Main program.
+	 */
+	public static void main(String[] args) throws Exception {
+		//Insure proper amount of arguments
+		if (args.length != 1) {
+			System.err.println("Usage: java PasswordCrack2" 
+				\+ "<databaseFile>");
+			//Were done here
+			return;
+		}
+		
+		File dictionaryFile = new File(args[0]);
+		
+		//Reader for reading the input file
+		BufferedReader reader;
+
+		//Read the database
+		try {
+			reader = new BufferedReader
+				(new FileReader(databaseFile));
+		}
+		
+		//File not found, tell the user and give up
+		catch(FileNotFoundException e) {
+			System.err.println("File " + databaseFile.getName() + 
+				" does not exist");
+			System.exit (1);
+			//End the program
+			return;
+		}
+
+		//Dictionary for storing the hashes
+		Dictionary dictionary  = new Dictionary();
+		
+		//The final size of the dictionary
+		int size = Math.pow(CHARACTERS, LENGTH)
+		//Let the dictionary know how many hashes to expect
+		dictionary.setCount(size);
+		
+		//Arraylist of User threads
+		ArrayList<Thread> users = new ArrayList<Thread>();
+		//Parse input and start thread group 2 (users)
+		try {
+			while((line = reader.readLine()) !=null) {
+				//Split into tokens at any whitespace
+				String[] tokens = line.split("\\s+");
+				//Create and run a new thread
+				Thread t = new Thread(new User(tokens[0], dictionary));
+				//Start the thread
+				t.start();
+				//Add the thread to the group
+				users.add(t);
+
+				
+			}
+		}
+		
+		//Something went wrong, give up
+		catch(IOException e) {
+			System.err.println("Error reading " + 
+				databaseFile.getName());
+			System.exit (1);
+		}
+		
+		
+		//Join all the threads
+		for (Thread t: users) {
+			t.join();
+		}
+
+		
+
+	}//End of constructor
+}//End of PasswordCrack
